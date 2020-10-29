52d4ab6bdbfb130b480921f181325dd090459f38
Ashish Pundir <pundir.pd@gmail.com>
2020-10-29 05:14:29 +0000

    goroutine.

--- goroutines/waitgroup.go
+++ goroutines/waitgroup.go
@@ -11,18 +11,18 @@ func goroutine1 (waitgroup *sync.WaitGroup) {
 }
 
 func goroutine2 (waitgroup *sync.WaitGroup) {
+    defer waitgroup.Done()
     fmt.Println("Inside my goroutine2")
-    //return
-    waitgroup.Done()
+    return
 }
 
 func main() {
     fmt.Println("Hello World")
 
     var waitgroup sync.WaitGroup
-    waitgroup.Add(1)
+    waitgroup.Add(2)
     go goroutine1(&waitgroup)
-    waitgroup.Add(1)
+    //waitgroup.Add(1)
     go goroutine2(&waitgroup)
     waitgroup.Wait()
 
